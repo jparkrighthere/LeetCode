8e5526ff6d17ea59396fd77f3123b1a63633e10d
Jeonghyeon Park <116504082+jparkrighthere@users.noreply.github.com>
2023-11-16 03:59:02 -0600

    Time: 0 ms (100.00%), Space: 43.4 MB (16.68%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (dfs(root) == -1) {
+            return false;
+        }
+        return true;
+    }
+
+    public int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+
+        if (left == -1) {
+            return -1;
+        }   
+
+        if (right == -1) { 
+            return -1;
+        }
+
+        if (Math.abs(left -right) > 1) {
+            return -1;
+        }
+
+        return 1 + Math.max(left,right);
+    }
+}
